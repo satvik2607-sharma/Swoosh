8198b9129f2e2c607cb188b7562e3d1a1c69f003
satvik sharma <satviksharma2607@gmail.com>
2023-12-03 15:56:51 +0530

    league -> skill selection

---
+++ app/src/main/java/com/example/swoosh/BaseActivity.kt
@@ -0,0 +1,44 @@
+package com.example.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "lifecycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "${javaClass.simpleName} OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "${javaClass.simpleName} OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "${javaClass.simpleName} OnResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "${javaClass.simpleName} OnRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "${javaClass.simpleName} OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "${javaClass.simpleName} OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "${javaClass.simpleName} OnDestroy")
+    }
+}
